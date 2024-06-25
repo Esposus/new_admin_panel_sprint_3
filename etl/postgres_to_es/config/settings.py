44c32018367b61773c7797cb913dd3e78a9e98d2
Dmitry <adm713934@yandex.ru>
2024-06-25 11:43:03 +0300

    added constants

--- etl/postgres_to_es/config/settings.py
+++ etl/postgres_to_es/config/settings.py
@@ -6,30 +6,36 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 load_dotenv()
 
 
-class PostgresSettings(BaseSettings):
+class SettingsPostgres(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
     DB_NAME: str
     DB_USER: str
     DB_PASSWORD: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_OPTIONS: str
+    DB_HOST: str = 'postgres_etl'
+    DB_PORT: int = 5432
+    DB_OPTIONS: str = '-c search_path=content'
 
 
-class ElasticSearchSettings(BaseSettings):
-    ES_HOST: str
-    ES_PORT: int
-    ES_INDEX: str
-    ES_SCHEMA: str
+class SettingsElasticSearch(BaseSettings):
+    ES_HOST: str = 'elasticsearch_etl'
+    ES_PORT: int = 9200
+    ES_INDEX: str = 'movies'
+    ES_SCHEMA: str = os.path.join('config', 'es_schema.json')
 
 
-class ETLSettings(BaseSettings):
-    LIMIT: int
-    STATE_FILE: str
-    TIME_SLEEP: int
+class SettingsETL(BaseSettings):
+    LIMIT: int = 100
+    STATE_FILE: str = 'state_file.json'
+    TIME_SLEEP: int = 5
+
+    FILM_WORK_TABLE = 'content.film_work'
+    PERSON_TABLE = 'content.person'
+    GENRE_TABLE = 'content.genre'
+    PERSON_FILM_WORK_TABLE = 'content.person_film_work'
+    GENRE_FILM_WORK_TABLE = 'content.genre_film_work'
 
 
-settings_postgres = PostgresSettings()
-settings_es = ElasticSearchSettings()
-settings_etl = ETLSettings()
+settings_postgres = SettingsPostgres()
+settings_es = SettingsElasticSearch()
+settings_etl = SettingsETL()
--- etl/postgres_to_es/postgres_and_es/queries_in_postgres.py
+++ etl/postgres_to_es/postgres_and_es/queries_in_postgres.py
@@ -1,31 +1,90 @@
 from config.settings import settings_etl
 
 
+FILM_WORK_TABLE = settings_etl.FILM_WORK_TABLE
+PERSON_TABLE = settings_etl.PERSON_TABLE
+GENRE_TABLE = settings_etl.GENRE_TABLE
+PERSON_FILM_WORK_TABLE = settings_etl.PERSON_FILM_WORK_TABLE
+GENRE_FILM_WORK_TABLE = settings_etl.GENRE_FILM_WORK_TABLE
+LIMIT = settings_etl.LIMIT
+
+
 def query_filmworks(timestamp) -> str:
+    """Запрос на получение обновлённых фильмов за заданный промежуток времени."""
     return f"""
         SELECT id, modified
-        FROM content.film_work
-        WHERE modified > {timestamp}
+        FROM {FILM_WORK_TABLE}
+        WHERE modified > '{timestamp}'
         ORDER BY modified
-        LIMIT {settings_etl.LIMIT}    
+        LIMIT {LIMIT};
     """
 
 
 def query_persons(timestamp) -> str:
+    """Запрос на получение обновлённых людей за заданный промежуток времени."""
     return f"""
         SELECT id, modified
-        FROM content.person
-        WHERE modified > {timestamp}
+        FROM {PERSON_TABLE}
+        WHERE modified > '{timestamp}'
         ORDER BY modified
-        LIMIT {settings_etl.LIMIT}    
+        LIMIT {LIMIT};
     """
 
 
 def query_genres(timestamp) -> str:
+    """Запрос на получение обновлённых жанров за заданный промежуток времени."""
     return f"""
         SELECT id, modified
-        FROM content.genre
-        WHERE modified > {timestamp}
+        FROM {GENRE_TABLE}
+        WHERE modified > '{timestamp}'
         ORDER BY modified
-        LIMIT {settings_etl.LIMIT}    
+        LIMIT {LIMIT};
+    """
+
+
+def query_filmworks_persons(persons: list) -> str:
+    """Запрос на получение фильмов с обновленными людьми."""
+    return f"""
+        SELECT fw.id, fw.modified
+        FROM {FILM_WORK_TABLE} fw
+        LEFT JOIN {PERSON_FILM_WORK_TABLE} pfw ON pfw.film_work_id=fw.id
+        WHERE pfw.person_id {f"IN {tuple(persons)}" if len(persons) > 1 else f"='{persons[0]}'"}
+        ORDER BY fw.modified
+        LIMIT {LIMIT};
+    """
+
+
+def query_filmworks_genres(genres: list) -> str:
+    """Запрос на получение фильмов с обновленными жанрами."""
+    return f"""
+        SELECT fw.id, fw.modified
+        FROM {FILM_WORK_TABLE} fw
+        LEFT JOIN {GENRE_FILM_WORK_TABLE} gfw ON gfw.film_work_id=fw.id
+        WHERE gfw.genre_id {f"IN {tuple(genres)}" if len(genres) > 1 else f"='{genres[0]}'"}
+        ORDER BY fw.modified
+        LIMIT {LIMIT};
+    """
+
+
+def query_filmworks_all(filmwork_ids: tuple) -> str:
+    """Запрос на получение всей недостающей информации о фильмах."""
+    return f"""
+        SELECT
+        fw.id as fw_id, 
+        fw.title, 
+        fw.description, 
+        fw.rating, 
+        fw.type, 
+        fw.created, 
+        fw.modified, 
+        pfw.role, 
+        p.id as person_id, 
+        p.full_name,
+        g.name as genre
+        FROM {FILM_WORK_TABLE} fw
+        LEFT JOIN {PERSON_FILM_WORK_TABLE} pfw ON pfw.film_work_id=fw.id
+        LEFT JOIN {PERSON_TABLE} p ON p.id=pfw.person_id
+        LEFT JOIN {GENRE_FILM_WORK_TABLE} gfw ON gfw.film_work_id=fw.id
+        LEFT JOIN {GENRE_TABLE} g ON g.id=gfw.genre_id
+        WHERE fw.id {f"IN {tuple(filmwork_ids)}" if len(filmwork_ids) > 1 else f"='{filmwork_ids[0]}'"};
     """
